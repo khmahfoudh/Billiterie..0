5f0d420ae238d2600004db228fdd247d73b88d79
WAEL2207 <Wailkyass23@gmail.com>
2025-04-10 16:56:19 +0200

    update1

---
+++ database/migrations/2025_04_10_145456_create_organisateur_evenements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organisateur_evenements', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('organisation')->nullable(); // nom de l'entité ou groupe qu'il représente
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organisateur_evenements');
+    }
+};
